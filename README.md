96819d8dbbf397cce576c6469d4c66d71bf0861e
Max.Shel <maxs200099@gmail.com>
2025-04-25 18:41:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+API Gateway / Rate limiter 
+
+Nginx -> API Gateway -> Pods 
+
+Configuration:
+
+1. Pods url
+2. Limit type (url, query_param, ip)
+3. Total tokens for limit type for a minute# rate_limiter
---
+++ test_proxy_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/mama')
+def hello_mama():
+    return 1 / 0
+
+
+app.run(debug=True, host='0.0.0.0', port=8000)
