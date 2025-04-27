ff1753a288d26eeafba4c12b797c7d34aeaa427c
Max.Shel <maxs200099@gmail.com>
2025-04-27 21:58:00 +0700

    Add redis: in_progress

--- src/limiter.rs
+++ src/limiter.rs
@@ -3,35 +3,39 @@ use std::net::{IpAddr, SocketAddr};
 use std::thread;
 use std::sync::{Arc, RwLock};
 use std::time::Duration;
-use axum::body::Body;
-use axum::extract::{ConnectInfo, Request as AxumRequest, State};
+use axum::body::{Body, Bytes};
+use axum::extract::{ConnectInfo, State};
 use axum::http::{Request, StatusCode};
 use axum::middleware::Next;
 use axum::response::{IntoResponse, Response};
+use axum_macros::debug_middleware;
+use deadpool_redis::{Config, Connection, Pool};
+
 use crate::settings::{PossibleStrategies, RateLimiterSettings};
 
+
+#[debug_middleware]
 pub async fn middleware(
     State(rate_limiter_manager): State<Arc<RateLimiterManager>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     request: Request<Body>,
     next: Next,
-) -> Response {
+) -> Response<Body> {
     if rate_limiter_manager.ip_whitelist.contains(&addr.ip()) {
         println!("IP {} is whitelisted", addr.ip());
-        return next.run(request).await
+        return next.run(request).await;
     }
-    
-    let mut is_too_many_requests = false;
-    for limiter in rate_limiter_manager.rate_limiters.iter() {
-        if !limiter.check(&request, ConnectInfo(addr)) {
-           is_too_many_requests = true;
-        }
-        
-        if is_too_many_requests {
-            return (StatusCode::TOO_MANY_REQUESTS, "Rate limit exceeded").into_response();
-        }
-        
-        dbg!(limiter);
+
+    let q_request: Request<Bytes> = Request::builder()
+        .uri(request.uri())
+        .method(request.method())
+        .body(Bytes::from("hello")) // TODO replace with request body
+        .unwrap();
+
+
+    if !rate_limiter_manager.rate_limiters[0].check(q_request).await {
+        println!("Rate limit exceeded for {}", addr.ip());
+        return (StatusCode::TOO_MANY_REQUESTS, "Rate limit exceeded").into_response();
     }
 
     next.run(request).await
@@ -50,42 +54,46 @@ impl Strategy {
             PossibleStrategies::URL => Strategy::Url(UrlRateLimiterStrategy),
         }
     }
-    
+
     fn check_limit(
         &self,
-        data: &mut HashMap<String, u32>,
+        redis_connection: Connection,
         default_limit: u32,
-        request: &AxumRequest,
-        addr: ConnectInfo<SocketAddr>,
+        request: Request<Bytes>,
+        addr: SocketAddr,
     ) -> bool {
         match self {
-            Strategy::IP(strategy) => strategy.check_limit(data, default_limit, request, addr),
-            Strategy::Url(strategy) => strategy.check_limit(data, default_limit, request, addr),
+            Strategy::IP(strategy) => strategy.check_limit(redis_connection, default_limit, request, addr),
+            Strategy::Url(strategy) => strategy.check_limit(redis_connection, default_limit, request, addr),
         }
     }
-    
+
 }
 
+#[derive(Clone, Debug)]
 pub struct RateLimiterManager {
     redis_addr: String,
     ip_whitelist: HashSet<IpAddr>,
-    rate_limiters: Vec<RateLimiter>,
+    rate_limiters: Vec<Arc<RateLimiter>>,
 }
 
 impl RateLimiterManager {
-    pub fn new(rate_limiter_settings: Arc<RateLimiterSettings>) -> Self {
+    pub fn new(rate_limiter_settings: Arc<RateLimiterSettings>) -> Result<Self, deadpool_redis::CreatePoolError> {
         let mut rate_limiters = Vec::new();
-        
+
+        let cfg = Config::from_url(format!("redis://{}", rate_limiter_settings.redis_addr.as_str()));
+        let pool = cfg.create_pool(Some(deadpool_redis::Runtime::Tokio1))?;
+
         for setting in rate_limiter_settings.limiters_settings.iter() {
             let strategy = Strategy::from_possible_strategy(&setting.strategy);
-            rate_limiters.push(RateLimiter::new(strategy, setting.tokens_count));
+            rate_limiters.push(Arc::new(RateLimiter::new(strategy, setting.tokens_count, pool.clone())));
         }
-        
-        Self {
+
+        Ok(Self {
             rate_limiters,
             redis_addr: rate_limiter_settings.redis_addr.clone(),
             ip_whitelist: rate_limiter_settings.ip_whitelist.clone(),
-        }
+        })
     }
 }
 
@@ -96,16 +104,18 @@ impl RateLimiterManager {
 struct RateLimiter {
     strategy: Strategy,
     default_limit: u32,
-    data: Arc<RwLock<HashMap<String, u32>>>
+    data: Arc<RwLock<HashMap<String, u32>>>,
+    redis_pool: Pool
 }
 
 
 impl RateLimiter {
-    pub fn new(strategy: Strategy, tokens_count: u32) -> Self {
+    pub fn new(strategy: Strategy, tokens_count: u32, redis_pool: Pool) -> Self {
         let limiter = Self {
             strategy,
             default_limit: tokens_count,
-            data: Arc::new(RwLock::new(HashMap::new()))
+            data: Arc::new(RwLock::new(HashMap::new())),
+            redis_pool,
         };
 
         let data = Arc::clone(&limiter.data);
@@ -122,18 +132,21 @@ impl RateLimiter {
 
         limiter
     }
+    
+    pub async fn check(&self, request: Request<Bytes>) -> bool {
 
-
-    pub fn check(&self, request: &AxumRequest, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> bool {
-        let mut data = self.data.write().unwrap();
-        self.strategy.check_limit(&mut data, self.default_limit, request, ConnectInfo(addr))
+        let redis_conn = match self.redis_pool.get().await {
+            Ok(redis_conn) => redis_conn,
+            Err(_) => return false
+        };
+        self.strategy.check_limit(redis_conn, self.default_limit, request, SocketAddr::new(IpAddr::V4("127.0.0.1".parse().unwrap()), 8080))
 
     }
 }
 
 
 pub trait RateLimiterChecker {
-    fn check_limit(&self, data: &mut HashMap<String, u32>, default_limit: u32, request: &AxumRequest, addr: ConnectInfo<SocketAddr>) -> bool;
+    fn check_limit(&self, redis_connection: Connection, default_limit: u32, request: Request<Bytes>, addr: SocketAddr) -> bool;
 }
 
 
@@ -144,31 +157,31 @@ pub struct UrlRateLimiterStrategy;
 
 
 impl RateLimiterChecker for IPRateLimiterStrategy {
-    fn check_limit(&self, data: &mut HashMap<String, u32>, default_limit: u32, _request: &AxumRequest, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> bool {
-        let ip = addr.ip().to_string();
-        if let Some(count) = data.get_mut(&ip) {
-            if *count == 0 {
-                return false
-            }
-            *count -= 1
-        } else {
-            data.insert(ip, default_limit - 1);
-        }
+    fn check_limit(&self, redis_connection: Connection, default_limit: u32, _request: Request<Bytes>, addr: SocketAddr) -> bool {
+        // let ip = addr.ip().to_string();
+        // if let Some(count) = data.get_mut(&ip) {
+        //     if *count == 0 {
+        //         return false
+        //     }
+        //     *count -= 1
+        // } else {
+        //     data.insert(ip, default_limit - 1);
+        // }
         true
-    }   
+    }
 }
 
 impl RateLimiterChecker for UrlRateLimiterStrategy {
-    fn check_limit(&self, data: &mut HashMap<String, u32>, default_limit: u32, request: &AxumRequest, ConnectInfo(_addr): ConnectInfo<SocketAddr>) -> bool {
-        let uri = request.uri().to_string();
-        if let Some(count) = data.get_mut(&uri) {
-            if *count == 0 {
-                return false
-            }
-            *count -= 1
-        } else {
-            data.insert(uri, default_limit - 1);
-        }
+    fn check_limit(&self, redis_connection: Connection, default_limit: u32, request: Request<Bytes>, _addr: SocketAddr) -> bool {
+        // let uri = request.uri().to_string();
+        // if let Some(count) = data.get_mut(&uri) {
+        //     if *count == 0 {
+        //         return false
+        //     }
+        //     *count -= 1
+        // } else {
+        //     data.insert(uri, default_limit - 1);
+        // }
         true
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -3,9 +3,9 @@ use std::sync::Arc;
 use axum::body::Body;
 use axum::extract::State;
 use axum::http::{Request, Response, StatusCode};
+use axum::middleware::{from_fn_with_state};
 use axum::response::IntoResponse;
 use axum::Router;
-use axum::middleware::from_fn_with_state;
 use axum::routing::any;
 use axum_proxy::AppendSuffix;
 use tower_service::Service;
@@ -15,31 +15,27 @@ use crate::settings::{ApiGatewaySettings, Settings};
 
 pub struct ProxyServer {
     settings: Settings
-    
 }
 
 impl ProxyServer {
-    
     pub fn new(settings: Settings) -> Self {
         Self {
             settings
         }
-        
     }
+
     pub async fn run(self) -> Result<(), std::io::Error>{
         let listener = tokio::net::TcpListener::bind(self.settings.api_gateway_settings.proxy_server_addr.clone())
             .await?;
 
-        
-        let limiter = RateLimiterManager::new(Arc::new(self.settings.rate_limiter_settings));
-        
+        let limiter = Arc::new(RateLimiterManager::new(Arc::new(self.settings.rate_limiter_settings)).unwrap());
         
         let app = Router::new()
             .route("/*path", any(handler))
             .route("/", any(handler))
-            .layer(from_fn_with_state(Arc::new(limiter), limiter::middleware))
+            .layer(from_fn_with_state(limiter, limiter::middleware))
             .with_state(Arc::new(self.settings.api_gateway_settings));
-        
+
         axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await
     }
 }
